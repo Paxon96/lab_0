d0c008c4df0537b5ad5367e67824f952ad6dbe7f
wloczyk.bartłomiej <209506@edu.p.lodz.pl>
2019-03-05 18:20:06 +0100

    fix remaining tests

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,18 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>();
+        if (value == null) {
+            return new ArrayList<>();
+        } else {
+            List<String> substrings = new ArrayList<>();
+
+            for (int i = 0; i <= value.length(); i++) {
+                substrings.add(value.substring(i));
+            }
+
+            return substrings;
+        }
+
     }
 
 }
